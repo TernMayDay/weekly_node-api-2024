305e1145ba49ddb488fb0e6df80b38405aea5321
puffy <tern8299@gmail.com>
2024-06-12 15:28:31 +0800

    測試view

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const cors = require('cors');
 // routes
 var postsRouter = require('./routes/posts');
 var usersRouter = require('./routes/users');
+var index = require('./routes/index');
 
 // express
 var app = express();
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use('/', indexRouter);
 app.use('/posts', postsRouter);
 app.use('/users', usersRouter);
+app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
